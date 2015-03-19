7a5164d20b3e9c6c95c7bff9c99761f601acf55b
Juanjo Tortosa <juanjo.tortosa@gmail.com>
2015-03-19 10:25:41 +0100

    security

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function(req, res, next){
-	return res.status(401).send('forbidden');
+//	return res.status(401).send('forbidden');
 	
 	req.mongoMng.serverInfo(function(err, info){
 		if(err)
