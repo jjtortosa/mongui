4b07e4994800dfd47239b72cada064acadba67af
Juanjo Tortosa <juanjo.tortosa@gmail.com>
2015-04-17 18:41:09 +0200

    mongodump & mongorestore

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ console.info('Config file "%s" loaded', file);
 
 var app = express();
 
-// view engine setup
 app.set('conf', conf);
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
