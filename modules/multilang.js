7d4866b3a37ca8acbad3796148f57a3a62c7812b
Juanjo Tortosa <juanjo.tortosa@gmail.com>
2015-04-21 17:04:23 +0200

    multilang enhacements

--- modules/multilang.js
+++ modules/multilang.js
@@ -1,9 +1,15 @@
 /* global module */
 
 module.exports = function multilang(req,res,next){
-	req.lang = req.acceptsLanguages('en','es') || 'en';
-	
+	if(req.query.changeLang){
+		req.session.lang = req.query.changeLang;
+
+		return res.redirect(req.path);
+	}
+
+	req.lang = req.session.lang || req.acceptsLanguages('en','es') || 'en';
+
 	res.locals.ml = require('../language/' + req.lang);
-	
+
 	next();
 };
